ebf2076290bdb19aaaaa3d0845c7a25c462c61c5
Никита Хромов <chromovnick@gmail.com>
2021-12-05 18:42:45 +0300

    Добавьте файлы проекта.

---
+++ 8_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("D:/Test/Numbers.txt");
+            Random random = new Random();
+            int[] array = new int[10];
+            int S = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = random.Next(0,10);
+                sw.WriteLine(array[i]);
+            }
+            sw.Close();
+            StreamReader sr = new StreamReader("D:/Test/Numbers.txt");
+            string[] readarray = File.ReadAllLines("D:/Test/Numbers.txt");
+            int[] intarray = new int [readarray.Length];
+            int Sum = 0;
+            for (int i = 0; i < readarray.Length; i++)
+            {
+                intarray[i] = int.Parse(readarray[i]);
+            }
+            S=intarray.Sum();
+            Console.WriteLine("{0}", S);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo("D:/Test");
+            DirectoryInfo[] darray = directory.GetDirectories();
+            Console.WriteLine("Вложенные файлы:{0}", directory.GetFiles());
+            foreach (DirectoryInfo d in darray)
+            { 
+                Console.WriteLine("Вложенные директории:{0}", d.Name);
+                d.GetFiles();
+                FileInfo[] files = d.GetFiles();
+                if (null != files)
+                {
+                    foreach (FileInfo file in files)
+                    {
+                        Console.WriteLine("файлы вложенных директорий:{0}", file.Name);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+         
